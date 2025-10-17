08d9af4711939cdbd4dd612a8d17b85e8f88c7b9
suhas152 <vedasuhaskulkurni@gmail.com>
2025-10-17 21:34:18 +0530

    v1.0

--- src/main/java/com/klef/labexam/SpringBootUserApplication.java
+++ src/main/java/com/klef/labexam/SpringBootUserApplication.java
@@ -2,9 +2,10 @@ package com.klef.labexam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootUserApplication {
+public class SpringBootUserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootUserApplication.class, args);
